c4d0702815a2cbc5d4b36b4f8881fa62143966fa
Regin99 <regintmor@gmail.com>
2022-04-23 08:18:07 +0300

    added firebase notifications

--- pages/Search/index.js
+++ pages/Search/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import {View, TextInput} from 'react-native';
+import {useSelector} from 'react-redux';
 import styles from './styles';
 
-const Search = ({navigation}) => {
+const Search = ({navigation, route}) => {
+  const searchInput = useRef(null);
+  const {theme} = useSelector(state => state.settings);
+
+  useEffect(() => {
+    if (route.params?.searchFocus) {
+      searchInput.current.focus();
+    }
+  }, [route, navigation]);
+
   return (
-    <View style={styles.container}>
-      <Text>SearchPage</Text>
+    <View style={[styles.container, styles.containerThemed[theme]]}>
+      <TextInput ref={searchInput} style={styles.search} placeholder="Search" />
     </View>
   );
 };
--- stacks/Main/index.js
+++ stacks/Main/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {useSelector} from 'react-redux';
+import messaging from '@react-native-firebase/messaging';
 
 import TabsStack from '../Tabs';
 //pages
@@ -10,13 +11,15 @@ import Settings from '../../pages/Settings';
 import BackArrow from '../../components/BackArrow/BackArrow';
 import {COLORS} from '../../constants/styleConstans';
 import styles from './styles';
+import {useNavigation} from '@react-navigation/native';
 
 const Stack = createNativeStackNavigator();
 
 const Main = () => {
+  const navigation = useNavigation();
   const {theme} = useSelector(state => state.settings);
 
-  const screensOptions = ({navigation}) => ({
+  const screensOptions = () => ({
     headerStyle: styles.themedBG[theme],
     headerTitleStyle: styles.headerTitle[theme],
     headerTitleAlign: 'center',
@@ -24,6 +27,16 @@ const Main = () => {
       <BackArrow onPress={navigation.goBack} size={35} color={COLORS.blue} />
     ),
   });
+
+  const navigateToSearch = () =>
+    navigation.navigate('Search', {searchFocus: true});
+
+  useEffect(() => {
+    messaging().onNotificationOpenedApp(remoteMessage => navigateToSearch());
+    messaging()
+      .getInitialNotification()
+      .then(remoteMessage => remoteMessage && navigateToSearch());
+  }, [navigation]);
 
   return (
     <Stack.Navigator>
